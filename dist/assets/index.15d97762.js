dced44dff2d600271aa7a25524141575253b19d0
jedlimlx <jedlimlx@outlook.com>
2024-03-24 17:34:25 +0800

    Deploy from branch instead of from github actions

---
+++ dist/assets/index.15d97762.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:77f0cd589051fa2ee096ab67c251f4cd7074acd3d4ca0607add35d11454db6be
+size 124614
---
+++ dist/assets/webfontloader.b777d690.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:41e8191f0539e5a73ff476600e260b30fca1a8abd5067a5e3c941ee5c810dd6f
+size 12716
