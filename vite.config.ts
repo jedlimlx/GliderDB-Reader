d3cfeef543d05b4eb37560e955585421717ab07c
jedlimlx <jedlimlx@outlook.com>
2024-04-10 10:46:38 +0800

    Deploy with Vercel instead

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/gliderdb-reader/",
+  base: "/",
   plugins: [
     vue({
       template: { transformAssetUrls }
